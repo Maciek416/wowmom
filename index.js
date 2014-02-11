aaa0b83503b82430e719c7ccc39ab0d76b0a16e6
Ryan Kois <ryan.kois@gmail.com>
2014-02-10 20:11:45 -0700

    Allow multiple m and w.

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
-var trans = {
-  'W': 'M',
-  'w': 'm',
-  'm': 'w',
-  'M': 'W'
-};
+function trans(letter){
+  var translation = {
+    'W': 'M',
+    'w': 'm',
+    'm': 'w',
+    'M': 'W'
+  };
+  return translation[letter];
+}
 
 module.exports = function(text) {
-  var regex = /(m|w)(\s*o\s*)(m|w)/gi;
+  var regex = /([mw]+)(\s*o\s*)([mw]+)/gi;
   return text.replace(regex, function(match, first, mid, last) {
-    return trans[first] + mid + trans[last];
+    firstTrans = first.split('').map(trans).join('');
+    lastTrans = last.split('').map(trans).join('');
+    return firstTrans + mid + lastTrans;
   });
 };
--- test.js
+++ test.js
@@ -1,6 +1,8 @@
 var wowmom = require('./index');
 
 console.log(wowmom('wow'));
+console.log(wowmom('wwwwwowwwwww'));
+console.log(wowmom('wmwmwowmwmwm'));
 console.log(wowmom('w o w'));
 console.log(wowmom('m o m'));
 console.log(wowmom('M O M'));
