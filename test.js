6e441a116f3e3cac75de734208cccac23fc9c860
Maciej Adwent <maciekadwent@gmail.com>
2014-02-11 21:12:03 -0800

    improved tests

--- test.js
+++ test.js
@@ -1,11 +1,11 @@
 var wowmom = require('./index');
 
-console.log(wowmom('wow'));
-console.log(wowmom('wwwwwowwwwww'));
-console.log(wowmom('wmwmwowmwmwm'));
-console.log(wowmom('w o w'));
-console.log(wowmom('m o m'));
-console.log(wowmom('M O M'));
-console.log(wowmom('W  O  W   M  O  M'));
-console.log(wowmom('wow really?'));
-console.log(wowmom('hi mom'));
+console.log(wowmom('wow') === 'mom');
+console.log(wowmom('wwwwwowwwwww') === 'mmmmmommmmmm');
+console.log(wowmom('wmwmwowmwmwm') === 'mwmwmomwmwmw');
+console.log(wowmom('w o w') === 'm o m');
+console.log(wowmom('m o m') === 'w o w');
+console.log(wowmom('M O M') === 'W O W');
+console.log(wowmom('W  O  W   M  O  M') === 'M  O  M   W  O  W');
+console.log(wowmom('wow really?') === 'mom really?');
+console.log(wowmom('hi mom') === 'hi wow');
