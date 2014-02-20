95815edccfe1ed7cefd50b4cf33c0b9a95d22ae6
Maciej Adwent <maciekadwent@gmail.com>
2014-02-19 20:55:19 -0800

    cleanup

--- test.js
+++ test.js
@@ -5,8 +5,6 @@ var wowmom = require('./index');
 test('wowmom', function (t) {
   t.equals(wowmom('wow'), 'mom');
   t.equals(wowmom('wwwwwowwwww'), 'mmmmmommmmm');
-  // This one no longer supported...
-  //t.equals(wowmom('wmwmwowmwmwm'), 'mwmwmomwmwmw');
   t.equals(wowmom('w o w'), 'm o m');
   t.equals(wowmom('m o m'), 'w o w');
   t.equals(wowmom('M O M'), 'W O W');
